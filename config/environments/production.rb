5638c321e9033ebe0a497e5298691ddc9f900695
adamjgoodman <goodmanadamj@gmail.com>
2020-05-04 22:37:16 -0400

    fix precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: ‘https://myxer.herokuapp.com/’}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -14,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { host: 'http://myxer.herokuapp.com' }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
